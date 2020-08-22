c0172d2eb9b349b9928ad11e2adbb8476ffbbaeb
sanjananair17 <sanjananair17@gmail.com>
2020-08-22 09:19:14 -0400

    Hopefully fixked Plinko Game

--- sketch.js
+++ sketch.js
@@ -1,34 +1,3 @@
-/*const Engine = Matter.Engine;
-const World= Matter.World;
-const Bodies = Matter.Bodies;
-const Constraint = Matter.Constraint;
-
-var engine, world;
-var particles = [];
-var plinkos = [];
-var divisions = [];
-
-var divisionHeight = 300;
-function setup() {
-  createCanvas(480,800);
-  engine = Engine.create();
-  world = engine.world;
-
-  ground = new Ground(240, 780, 500, 20);
-}
-
-function draw() {
-  background(0);
-  Engine.update(engine); 
-
-  ground.display();
-
-  drawSprites();
-
-  for(var k = 0, k <=width; k = k + 80) {
-    divisions.push(new Division(k, height-divisionHeight/2, 10, divisionHeight));
-  }
-}*/
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Events = Matter.Events;
@@ -48,8 +17,7 @@ function setup() {
   
   engine = Engine.create();
   world = engine.world;
-  ground = new Ground(200,800,1700,10);
-  ground = new Ground(200, 795, 1700, 20)
+  ground = new Ground(200,800,1700,30);
 
   for(var j = 40; j<= width; j = j+50){
     plinkos.push(new Plinko(j , 75))
@@ -82,34 +50,27 @@ function setup() {
 
 function draw() {
   if(frameCount % 60 === 0){
-    particle = new Particle(random(120, 500), 0, 7, random(0, 360));
+    particle = new Particle(random(120, 500), 0, 7)
     particles.push(particle);
   }
 
   background("black");
+
   Engine.update(engine, 25);
+
   for(var i = 0; i < particles.length; i++){
     particles[i].display();
   }
 
- 
-  
-
-
-
-
-
-
   for(var k = 0; k<division.length;k++){
     division[k].display();
- }
-
+  }
 
   for(var j = 0; j<plinkos.length;j++){
     plinkos[j].display();
- }
+  }
 
-ground.display();
+  ground.display();
 
-drawSprites();
+  drawSprites();
 }
